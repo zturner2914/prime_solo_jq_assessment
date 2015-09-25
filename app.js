d54785cc382602931e800f96440925a5a82f47af
Zachary <zturner29@gmail.com>
2015-09-24 22:03:11 -0500

    remove button function, minor tweak

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ $(document).ready(function () {
 	var numberOfClicks = 0;
 	var lineNumber = 0;
 	$(".addDivButton").on('click', function(){
-		//numberOfClicks++;
-		//lineNumber++;
-		//var $myContent = $("<li>" + "Line #" + lineNumber + " " + "clicked" + " " + numberOfClicks + " " + "times" + " " + "<button class='changeColor'>Change Color</button>" + " " +
-		//	"<button class='removeButton'>Remove</removeButton>" + "<li>");
-		//$("ul").append($myContent);
-
-		//document.createElementbyId("myDIV");
-		//function myDivCreator () {
 		numberOfClicks++;
 		lineNumber++;
-		var $newDiv  = document.createElement('DIV');
-		var $myContent = document.createTextNode ($("<li>" + "Line #" + lineNumber + " " + "clicked" + " " + numberOfClicks + " " + "times" + " " + "<button class='changeColor'>Change Color</button>" + " " +
-			"<button class='removeButton'>Remove</removeButton>" + "<li>"));
-		$newDiv.appendChild($myContent);
-		document.body.append($newDiv);
+		var $myContent = $("<div>" + "Line #" + lineNumber + " " + "clicked" + " " + 
+			numberOfClicks + " " + "times" + " " + "<br>" + 
+			"<button class='changeColor'>Change Color</button>" + " " +
+			"<button class='removeButton'>Remove</removeButton>" + "</div>");
+		$("body").append($myContent);
+	});
+
+		$(document).on("click", '.removeButton', function () {
+			$(this).parent().remove();
+
+		});
+
+		$(document).on('click', '.changeColor', function () {
+
+		});
+
 			
 	});
-});
